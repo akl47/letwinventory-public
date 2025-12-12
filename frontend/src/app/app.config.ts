6abe6f9cf245b4adeaa30a0ae9000a4cd9b44910
akl47 <alexanderletwin@gmail.com>
2025-12-11 20:18:49 -0800

    I have no idea what im doing

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,3 +1,12 @@
-export const BASE_URL = 'http://localhost:3000';
-export const TOKEN_KEY = 'auth_token';
-export const NAME_KEY = 'name';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
+};
